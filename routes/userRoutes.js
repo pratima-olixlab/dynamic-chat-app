ed38907d0c00060edf6ce51a35f810fa15807055
pratima-olixlab <159017898+pratima-olixlab@users.noreply.github.com>
2024-02-26 11:25:34 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -24,7 +24,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination:function(req, file, cb){
-       cb(null, path.join(__dirname,'../public/images'));
+        cb(null, '/tmp');
     },
     filename:function(req, file, cb){
         const name = Date.now()+'-'+file.originalname;
