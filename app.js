af63c3b69130f94bf2814c83b7056f644c4c6a67
pratima-olixlab <159017898+pratima-olixlab@users.noreply.github.com>
2024-02-26 10:09:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/dynamic-chat-app');
 const app = require('express')();
+app.set('views', './views');
 const http = require('http').Server(app);
 const userRoute = require('./routes/userRoutes');
 
